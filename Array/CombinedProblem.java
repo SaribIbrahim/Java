64464bffa3cbb1229cf674d12b4faac5384e3398
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-09-10 11:41:45 +0500

    Fixed a bug

--- Array/CombinedProblem.java
+++ Array/CombinedProblem.java
@@ -1,96 +1,103 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class CombinedProblem {
-    public static void main(String[] args){
-        
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter how many elements you want in array: ");
-        int num=sc.nextInt();
-        int[] arr=new int[num];
+        int num = sc.nextInt();
+        int[] arr = new int[num];
 
         System.out.println("Enter elements for array");
-        for(int i=0;i<arr.length;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println("Array is: "+Arrays.toString(arr));
-        
-        //zereos at end
-        int index=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0){
-                arr[index]=arr[i];
+        System.out.println("Array is: " + Arrays.toString(arr));
+
+        // zereos at end
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
                 index++;
             }
         }
-        while(index<arr.length){
-            arr[index]=0;
+        while (index < arr.length) {
+            arr[index] = 0;
             index++;
         }
-        System.out.println("Array with 0's at end is: "+Arrays.toString(arr));
+        System.out.println("Array with 0's at end is: " + Arrays.toString(arr));
 
-        //reversing the array
-        int left=0;
-        int right=arr.length-1;
+        // reversing the array
+        int left = 0;
+        int right = arr.length - 1;
         int temp;
-        while(left<right){
-            temp=arr[left];
-            arr[left]=arr[right];
-            arr[right]=temp;
+        while (left < right) {
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
             left++;
             right--;
         }
         System.out.println("Array after reversal is: " + Arrays.toString(arr));
 
-        //min number
-        int min=arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<min){
-                min=arr[i];
+        // min number
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
         System.out.println("Minimum number is: " + min);
 
-        //max number
-        int max=arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
+        // max number
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
         System.out.println("Maximum number is: " + max);
 
-        //second largest
-        int largest=Integer.MIN_VALUE;
-        int secondLargest=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>largest){
-                secondLargest=largest;
-                largest=arr[i];
-            }
-            else if(arr[i]>secondLargest && arr[i]<largest){
-                secondLargest=arr[i];
+        // second largest
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] < largest) {
+                secondLargest = arr[i];
             }
         }
         System.out.println("Second largest number is: " + secondLargest);
 
-        //palindrome
-        int start=arr[0];
-        int end=arr.length-1;
-        while(start<end){
-            if(arr[start]!=arr[end]){
-                System.out.println("Array is not a palindrome");
+        // palindrome
+        int start = arr[0];
+        int end = arr.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
                 break;
             }
-            System.out.println("Array is a palindrome");
+            start++;
+            end--;
         }
-       
-        //factorial
+        if (isPalindrome) {
+            System.out.println("Array is a palindrome");
+        } else {
+            System.out.println("Array is not a palindrome");
+        }
+
+        // factorial
         System.out.println("Enter a number to find factorial: ");
-        int n=sc.nextInt();
-        int fact=1;
-        for(int i=1;i<=n;i++){
-            fact=fact*i;
+        int n = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
         }
         System.out.println("Factorial of " + n + " is " + fact);
         sc.close();
