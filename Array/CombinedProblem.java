6041a741997d98e21ac768f8cb8ac5ee4f275cb3
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-09-10 08:45:57 +0500

    Maximum number problem

--- Array/CombinedProblem.java
+++ Array/CombinedProblem.java
@@ -38,5 +38,14 @@ public class CombinedProblem {
             }
         }
         System.out.println("Minimum number is: " + min);
+
+        //max number
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Maximum number is: " + max);
     }
 }
