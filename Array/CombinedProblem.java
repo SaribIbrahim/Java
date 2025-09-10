7231ae8021f2447a697e148b3bd1179f67acea48
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-09-10 08:38:22 +0500

    Combined problem of array

---
+++ Array/CombinedProblem.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+public class CombinedProblem {
+    public static void main(String[] args){
+        int[] arr={3, 0, 5, 0, 2, 2, 5};
+        System.out.println("Array is: "+Arrays.toString(arr));
+        //zereos at end
+        int index=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                arr[index]=arr[i];
+                index++;
+            }
+        }
+        while(index<arr.length){
+            arr[index]=0;
+            index++;
+        }
+        System.out.println("Array with 0's at end is: "+Arrays.toString(arr));
+
+        //reversing the array
+        int left=0;
+        int right=arr.length-1;
+        int temp;
+        while(left<right){
+            temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+        System.out.println("Array after reversal is: " + Arrays.toString(arr));
+    }
+}
