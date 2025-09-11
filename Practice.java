29d5de556fe2dc05fda5479135647a927e7d3da1
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-09-11 10:15:31 +0500

    Maximum number(Practice)

--- Practice.java
+++ Practice.java
@@ -40,7 +40,7 @@ public class Practice {
         }
         System.out.println("Array after reversal is: "+Arrays.toString(arr));
 
-        //min
+        //min number
         int min=arr[0];
         for(int i=0;i<arr.length;i++){
             if(arr[i]<min){
@@ -49,7 +49,15 @@ public class Practice {
         }
         System.out.println("Minimum number is: "+min);
 
-        
+        //max number
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Maximum number is: "+max);
+
         sc.close();
     }
 }
