fb98630a4962d66899d0017f1d84a43f8100666a
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-09-11 21:28:49 +0500

    Palindrome problem(Practice)

--- Practice.java
+++ Practice.java
@@ -27,74 +27,91 @@ public class Practice {
         }
         System.out.println("Array with 0's at end is: " + Arrays.toString(arr));
 
-        //reverse array
-        int left=0;
-        int right=arr.length-1;
+        // reverse array
+        int left = 0;
+        int right = arr.length - 1;
         int temp;
-        while(left<right){
-            temp=arr[left];
-            arr[left]=arr[right];
-            arr[right]=temp;
+        while (left < right) {
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
             left++;
             right--;
         }
-        System.out.println("Array after reversal is: "+Arrays.toString(arr));
+        System.out.println("Array after reversal is: " + Arrays.toString(arr));
 
-        //min number
-        int min=arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<min){
-                min=arr[i];
+        // min number
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        System.out.println("Minimum number is: "+min);
+        System.out.println("Minimum number is: " + min);
 
-        //max number
-        int max=arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
+        // max number
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
-        System.out.println("Maximum number is: "+max);
+        System.out.println("Maximum number is: " + max);
 
         // second largest number
-        int largest=Integer.MIN_VALUE;
-        int secondLargest=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>largest){
-                secondLargest=largest;
-                largest=arr[i];
-            }
-            else if(arr[i]>secondLargest && arr[i]<largest){
-                secondLargest=arr[i];
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] < largest) {
+                secondLargest = arr[i];
             }
         }
-        System.out.println("Second largest number is: "+secondLargest);
+        System.out.println("Second largest number is: " + secondLargest);
 
         // array sort ascending
-        boolean isSorted=true;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]<arr[i-1]){
-                isSorted=false;
+        boolean isSorted = true;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                isSorted = false;
                 break;
             }
         }
-        if(isSorted){
-            System.out.println("Array "+Arrays.toString(arr)+" is sorted in ascending order");
-        }
-        else{
-            System.out.println("Array "+Arrays.toString(arr)+" is not sorted in ascending order");
+        if (isSorted) {
+            System.out.println("Array " + Arrays.toString(arr) + " is sorted in ascending order");
+        } else {
+            System.out.println("Array " + Arrays.toString(arr) + " is not sorted in ascending order");
         }
 
-        //factorial
+        // factorial
         System.out.println("Enter a number to find factorial: ");
-        int num=sc.nextInt();
-        int fact=1;
-        for(int i=1;i<=num;i++){
-            fact=fact*i;
+        int num = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
         }
-        System.out.println("Factorial of "+num+" is: "+fact);
+        System.out.println("Factorial of " + num + " is: " + fact);
+
+        // palindrome
+        int leftt = 0;
+        int rightt = arr.length - 1;
+        boolean isPalindrome = true;
+        while (leftt < rightt) {
+            if (arr[leftt] != arr[rightt]) {
+                isPalindrome = false;
+                break;
+            }
+            leftt++;
+            rightt--;
+        }
+        if(isPalindrome){
+            System.out.println("Array "+Arrays.toString(arr)+" is palindrome");
+        }
+        else{
+            System.out.println("Array "+ Arrays.toString(arr)+ " is not palindrome");
+        }
 
         sc.close();
     }
