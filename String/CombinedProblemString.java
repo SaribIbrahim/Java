0aacb8d38880d191129d055ace19046ca8f75777
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-09-12 10:45:27 +0500

    Character frequency problem

--- String/CombinedProblemString.java
+++ String/CombinedProblemString.java
@@ -18,6 +18,23 @@ public class CombinedProblemString {
             System.out.println("String " + str + " is not a palindrome");
         }
 
+        // character frequency
+        int[] freq=new int[26];
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch>='a' && ch<='z'){
+                freq[ch-'a']++;
+            }
+        }
+        System.out.println("Character frequencies:");
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                char letter=(char)(i+'a');
+                System.out.println(letter+" = "+freq[i]);
+            }
+        }
+
+
         sc.close();
     }
 }
