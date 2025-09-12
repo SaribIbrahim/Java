3ae341ceb7c1455512f080019bd219fde1bd9cc2
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-09-12 12:44:51 +0500

    Anagram problem

--- String/Anagram.java
+++ String/Anagram.java
@@ -1,42 +1,37 @@
 import java.util.Scanner;
+
 public class Anagram {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter String 1: ");
-        String str1=sc.nextLine().toLowerCase();
+        System.out.println("Enter first string: ");
+        String str1 = sc.nextLine().toLowerCase();
 
-        System.out.println("Enter String 2: ");
-        String str2=sc.nextLine().toLowerCase();
+        System.out.println("Enter second string: ");
+        String str2 = sc.nextLine().toLowerCase();
 
-        if(str1.length()!= str2.length()){
+        StringBuilder sb = new StringBuilder(str2);
+
+        if (str1.length() != str2.length()) {
             System.out.println("Strings are not anagrams");
             sc.close();
             return;
         }
 
-        StringBuilder sb=new StringBuilder(str2);//Convert str2 to mutable form
-        boolean isAnagram=true;
-
-        for(int i=0;i<str1.length();i++){ //Check each char of str1
-            char ch=str1.charAt(i);
-            int index=sb.indexOf(String.valueOf(ch)); // checks ch of str1 in str2 and returns index if found
-            if(index!=-1){ //-1 means ch is not found
-                sb.deleteCharAt(index); // remove matched character
-            }
-            else{
-                isAnagram=false;
-                break;
+        for (int i = 0; i < str1.length(); i++) {
+            char ch = str1.charAt(i);
+            int index = sb.indexOf(String.valueOf(ch));
+            if (index != -1) {
+                sb.deleteCharAt(index);
+            } else {
+                System.out.println("Strings are not anagrams");
+                sc.close();
+                return;
             }
         }
-
-        if(isAnagram){
+        
+        if (sb.length() == 0) {
             System.out.println("Strings are anagrams");
         }
-        else{
-            System.out.println("Strings are not anagrams");
-        }
-
-        sc.close();
     }
 }
